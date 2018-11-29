8f7b89b742aee0ed68a0ea2bda9b1c4699b2e667
digio82 <45122282+digio82@users.noreply.github.com>
2018-11-29 03:45:50 +0000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 22:05:48 2018
+
+@author: FabioDiGiovine
+"""
+
+import unittest
+
+from ProcessChanges import get_commits, read_file
+
+class TestCommits(unittest.TestCase):
+
+    def setUp(self):
+        self.data = read_file('changes_python.log')
+
+    def test_number_of_lines(self):
+        self.assertEqual(5255, len(self.data))
+
+    def test_number_of_commits(self):
+        commits = get_commits(self.data)
+        self.assertEqual(422, len(commits))
+        self.assertEqual('Thomas', commits[0]['author'])
+        self.assertEqual('Jimmy', commits[420]['author'])
+        self.assertEqual(['FTRPC-500: Frontier Android || Inconsistencey in My Activity screen',
+				'Client used systemAttribute name="Creation-Date" instead of versionCreated as version created.'],
+				commits[24]['comment'])
+        self.assertEqual(['M /cloud/personal/client-international/android/branches/android-15.2-solutions/libs/model/src/com/biscay/client/android/model/util/sync/dv/SyncAdapter.java'],
+                commits[20]['changed_path'])
+
+if __name__ == '__main__':
+    unittest.main()
